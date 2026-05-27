261b689f6139231ef400115ab407b02a622ac1cc
melpon <shigemasa7watanabe+github@gmail.com>
2026-05-28 00:06:42 +0900

    修正

--- src/device_common.rs
+++ src/device_common.rs
@@ -156,7 +156,7 @@ impl DeviceListInner {
         // SAFETY: C 側が出力パラメータに有効なポインタを書き込むことを期待する。
         // devices_ptr が NULL でない場合、呼び出し側が free_devices で解放する責任を負う。
         let ret = unsafe { (ops.enumerate_devices)(&mut devices_ptr, &mut count) };
-        if ret < 0 || device_ptr.is_null() {
+        if ret < 0 || devices_ptr.is_null() {
             return Err(Error::DeviceAccessDenied);
         }
 
